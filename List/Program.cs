0235de6651f3dba923a21f36d45d138526b30171
siwar said <siwarsaid8@gmail.com>
2024-04-22 12:33:09 +0200

    quetesc# liste

---
+++ List/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> chaine = new List<string>();
+            chaine.Add("je suis siwar");
+            chaine.Add("je suis en formation");
+            chaine.Add("avec wild");
+            chaine.RemoveAt(0);
+            chaine.Remove("avec wild");
+            chaine[0] = "Je suis en formation avec wild school";
+
+            Console.WriteLine("affiche la liste : ");
+            foreach (string ch in chaine)
+            {
+                Console.WriteLine(ch);
+            }
+        }
+    }
+}
